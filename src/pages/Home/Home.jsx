c37c60d4e920aa549daeda264c569486312f2f23
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-09 02:24:12 +0600

    fundamentals

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import About from '@/components/Sections/About/About';
+import Fundamentals from '@/components/Sections/Fundamentals/Fundamentals';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <About />
+      <Fundamentals />
     </div>
   );
 };
