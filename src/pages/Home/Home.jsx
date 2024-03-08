8daf300650dbc9c2b4075e0e7c07581fcce68800
Protap Ghosh <protapg518@gmail.com>
2024-03-09 03:07:55 +0600

    added banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import About from '@/components/Sections/About/About';
+import Banner from '@/components/Sections/Banner/Banner';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <About />
     </div>
   );
