dd1cde479cdde44952e9766726a1c30290412216
Protap Ghosh <protapg518@gmail.com>
2024-03-09 18:54:11 +0600

    completed footer section

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import img from "@/assets/line.png"
 
 const Footer = () => {
   return (
     <div>
-      <h3>This is Footer area</h3>
+      <img src={img} alt="Image" />
+      <div className='max-w-[1620px] mx-auto 2xl:px-28 xl:px-28 md:px-16 sm:px-2 px-4 mt-7 mb-12'>
+        <p className='text-black text-center text-[14px] 2xl:text-[16px] xl:w-[91%] 2xl:w-[84%] mx-auto'>$MIDCURVE is a meme coin with no intrinsic value or expectation of financial return. There is no formal team or roadmad. the coin is completely useless and for entertainment purposes only.</p>
+      </div>
     </div>
   );
 };
