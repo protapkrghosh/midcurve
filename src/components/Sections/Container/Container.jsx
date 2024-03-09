36ff0741ea1ec31dd51d1892f9a924634ac7d541
Protap Ghosh <protapg518@gmail.com>
2024-03-09 14:29:56 +0600

    added second section

--- src/components/Sections/Container/Container.jsx
+++ src/components/Sections/Container/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
   return (
-    <div className="max-w-[1620px] mx-auto 2xl:px-36 xl:px-32 md:px-16 sm:px-2 px-4">
+    <div className="max-w-[1620px] mx-auto 2xl:px-28 xl:px-28 md:px-16 sm:px-2 px-4">
       {children}
     </div>
   );
--- src/components/Sections/Fundamentals/Fundamentals.jsx
+++ src/components/Sections/Fundamentals/Fundamentals.jsx
@@ -12,12 +12,36 @@ const Fundamentals = () => {
     <div className='pb-40'>
       <p className='text-[33px] xl:text-[50px] 2xl:text-[60px] text-center mb-[155px]'>The Fundamentals</p>
 
-      {/* first card */}
+      {/* First section */}
       <div className='bg-[#F8E2F6] rounded-md flex mb-[213px]'>
         <div className='bg-no-repeat bg-left bg-contain md:ml-[90px] lg:ml-[100px] xl:ml-[120px]' style={{ backgroundImage: `url(${fundaIcon1})` }}>
           <div className='flex flex-col-reverse md:flex-row justify-between items-center mt-[-55px] lg:mt-[-70px] xl:mt-[-95px] md:pt-3 lg:pt-0'>
-            <p className='w-full md:w-[62%] xl:w-[40%] 2xl:w-full text-[14px] md:text-[15px] lg:text-[17px] xl:text-[18px] 2xl:text-[26px] px-5 md:px-0 md:ml-[-40px] lg:mr-[100px] mt-8 md:mt-[7%] lg:mt-[8.5%] pb-10 md:pb-0'>Self-aware meme coin catering to the untapped potential of the mid-curve market. Bullish!</p>
-            <img src={funda1} alt="funda1" className='w-[85%] md:w-[42%] lg:w-[47%] 2xl:w-[45%] ml-[20px]' />
+            <p className='w-full md:w-[62%] xl:w-[40%] 2xl:w-[98%] text-[14px] md:text-[15px] lg:text-[17px] xl:text-[18px] 2xl:text-[24px] px-5 md:px-0 md:ml-[-40px] lg:mr-[100px] mt-10 md:mt-[7%] lg:mt-[8.5%] 2xl:mt-[7%] pb-10 md:pb-0'>Self-aware meme coin catering to the untapped potential of the mid-curve market. Bullish!</p>
+
+            <img src={funda1} alt="funda1" className='w-[85%] md:w-[42%] lg:w-[47%] 2xl:w-full ml-[20px]' />
+          </div>
+        </div>
+      </div>
+
+      {/* Second section */}
+      <div className='bg-[#E1E2F6] rounded-md flex mb-[213px]'>
+        <div className='bg-no-repeat md:bg-right bg-contain md:mr-[90px] lg:mr-[100px] xl:mr-[120px]' style={{ backgroundImage: `url(${fundaIcon2})` }}>
+          <div className='flex flex-col-reverse md:flex-row-reverse justify-between items-center mt-[-55px] lg:mt-[-95px] xl:mt-[-90px] md:pt-3 lg:pt-7'>
+            <p className='w-full md:w-[55%] lg:w-[49%] xl:w-[54%] 2xl:w-[98%] text-[14px] md:text-[15px] lg:text-[17px] xl:text-[18px] 2xl:text-[24px] md:text-end px-5 md:px-0 md:mr-[-40px] mt-12 md:mt-[7%] 2xl:mt-[7%] lg:mt-[8.5%] pb-10 md:pb-0'>Though I'm at the end of the curve, I recognize that the middle is where most people find themselves. $MIDCURVE is about giving a nod to the majority, making sure they all feel seen valued.</p>
+
+            <img src={funda2} alt="funda2" className='w-[85%] md:w-[42%] lg:w-[43%] 2xl:w-[85%]' />
+          </div>
+        </div>
+      </div>
+
+
+      {/* Third section */}
+      <div className='bg-[#F8EFE2] rounded-md flex mb-[213px]'>
+        <div className='bg-no-repeat bg-left bg-contain md:ml-[90px] lg:ml-[100px] xl:ml-[120px]' style={{ backgroundImage: `url(${fundaIcon3})` }}>
+          <div className='flex flex-col-reverse md:flex-row justify-between items-center mt-[-55px] lg:mt-[-70px] xl:mt-[-95px] md:pt-3 lg:pt-0'>
+            <p className='w-full md:w-[62%] xl:w-[40%] 2xl:w-[98%] text-[14px] md:text-[15px] lg:text-[17px] xl:text-[18px] 2xl:text-[24px] px-5 md:px-0 md:ml-[-40px] lg:mr-[100px] 2xl:mr-[160px] mt-10 md:mt-[7%] lg:mt-[8.5%] 2xl:mt-[7%] pb-10 md:pb-0'>A memecoin for the middle crowd. Bullish!</p>
+
+            <img src={funda3} alt="funda3" className='w-[85%] md:w-[42%] lg:w-[47%] 2xl:w-full ml-[20px] lg:mr-[-80px]' />
           </div>
         </div>
       </div>
